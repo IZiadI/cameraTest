5e70efaae5408c93da403b8154c75a6b086827e8
IZiadI <ziadashraf727@gmail.com>
2024-01-07 05:44:50 +0200

    resolution test

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "MVB-Phlex_excerciseConstructor-0.1";
+const contentToCache = [
+    "Build/excercise_constructor.loader.js",
+    "Build/excercise_constructor.framework.js",
+    "Build/excercise_constructor.data",
+    "Build/excercise_constructor.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
